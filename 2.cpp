fd5cb8d489821a235468249f3bb5f8f51ba63298
kingtirtho-ai <kingtirtho66@gmail.com>
2026-04-28 11:08:15 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <sstream>
-#include <ctime>
 using namespace std;
+int main ()
+{
+	cout<<"\n ____________________________________ \n";
+	cout<<"\n|                                    |\n";
+	cout<<"\n|          Benvenuto nel             |\n";
+	cout<<"\n|        registro elettrico!         |\n";
+	cout<<"\n|____________________________________| \n";
+	return 0;
+}
