41c60fddba73423a298f1417962ebc8772c4dff6
tangsan97912011-tech <tang.san9791.2011@gmail.com>
2026-04-28 11:10:45 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main ()
 {
-	cout<<"\n ____________________________________ \n";
-	cout<<"\n|                                    |\n";
-	cout<<"\n|          Benvenuto nel             |\n";
-	cout<<"\n|        registro elettrico!         |\n";
-	cout<<"\n|____________________________________| \n";
+	cout<<" ____________________________________ \n";
+	cout<<"|                                    |\n";
+	cout<<"|          Benvenuto nel             |\n";
+	cout<<"|        registro elettrico!         |\n";
+	cout<<"|____________________________________|\n";
 	return 0;
 }
