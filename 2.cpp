16025a56ebcf9390c5093f0ed4bd25f792235bda
tangsan97912011-tech <tang.san9791.2011@gmail.com>
2026-04-28 11:20:41 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -7,5 +7,8 @@ int main ()
 	cout<<"|          Benvenuto nel             |\n";
 	cout<<"|        registro elettrico!         |\n";
 	cout<<"|____________________________________|\n";
+	system("PAUSE");
+	sleep(1);
+	system("cls");
 	return 0;
 }
