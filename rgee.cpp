31ca52e73c231e18c6d20677a86627d2249879bc
kingtirtho-ai <kingtirtho66@gmail.com>
2026-05-12 11:06:46 +0200

    Create rgee.cpp

---
+++ rgee.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <ctime>
+using namespace std;
+int main ()
+{
+	cout<<" ____________________________________ \n";
+	cout<<"|                                    |\n";
+	cout<<"|          Benvenuto nel             |\n";
+	cout<<"|        registro elettrico!         |\n";
+	cout<<"|____________________________________|\n";
+	sleep(1);
+	system("cls");
+	string sceltauser;
+	string accountstudenti;
+	string accountprof;
+	cout<<"Vuoi accedere come studente oppure professore";
+	cin>>sceltauser;
+	if (sceltauser="studente");
+	cout<<"\n User: \n"
+	cin
+	cout<<"\n ora metti il tuo user \n";
+	cout<<"\n Login: \n";
+	cin>>user;
+	if (user==)
+	
+	return 0;
+}
