323ecca87e77999d025a1c62368e0663c3dd42db
kingtirtho-ai <kingtirtho66@gmail.com>
2026-05-12 11:40:15 +0200

    Update rewwer.cpp

--- rewwer.cpp
+++ rewwer.cpp
@@ -18,11 +18,15 @@ int main ()
 	cin>>user;
 	string password;
 	while (user!="tchandradas" || user!="vkumar" || user!="gnatalizi" || user!="mbilello" || user!="psuiarko" || user!="mjovanovic" || user!="amizzan" || user!="nfiroz" || user!="gsingh" || user!="cmayer" || user!="dbuleandra" || user!="aorlandi" || user!="sdomeniconi" || user!="bpina" || user!="cmihalas" || user!="ecischele" || user!="jzadipohindri" || user!="dschiavo" || user!="esandri" || user!="ebelletto" || user!="dpasin" ); //scrivi i nomi dei studenti //puoi controllare voti //generati da random
-	printf("\a"); 
+	printf("\a"); // a casa prova a vedere altri suoni
 	sleep(1);	
 	cout<<"\n Errore, utente sbagliato! \n";
 	system("color B8") //scegli il color rosso a casa
 	sleep(1);
 	system("cls");
+	if (user="tchandradas" || user="vkumar" || user="gnatalizi" || user="mbilello" || user="psuiarko" || user="mjovanovic" || user="amizzan" || user="nfiroz" || user="gsingh" || user="cmayer" || user="dbuleandra" || user="aorlandi" || user="sdomeniconi" || user="bpina" || user="cmihalas" || user="ecischele" || user="jzadipohindri" || user="dschiavo" || user="esandri" || user="ebelletto" || user="dpasin" ); //assenze etc. tabella. note. compiti. verifiche (nella tabella si puo amndare in avanti e con random vengono quelle robe li). media voti. 
+	
+	else (user=)
+	
 	return 0;
 }
